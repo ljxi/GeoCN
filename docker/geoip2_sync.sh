72a53edec478f4f8993c61199f6fac7b1ba376ad
alllowMultiQueries <asd1161575755@outlook.com>
2024-10-18 13:06:08 +0800

    Update geoip2_sync.sh

--- docker/geoip2_sync.sh
+++ docker/geoip2_sync.sh
@@ -13,6 +13,14 @@ echo "下载 GeoLite2-City.mmdb..."
 curl -L -o "GeoLite2-City.mmdb.tar.gz" \
   "${BASE_URL}?edition_id=GeoLite2-City&license_key=${LICENSE_KEY}&suffix=tar.gz"
 
+if [ $? -eq 0 ]; then
+  tar -xvzf GeoLite2-City.mmdb.tar.gz --strip-components=1 && rm GeoLite2-City.mmdb.tar.gz
+  echo "GeoLite2-City.mmdb 已成功下载并提取。"
+else
+  echo "GeoLite2-City.mmdb 下载失败。"
+  exit 1
+fi
+
 echo "下载 GeoLite2-ASN.mmdb..."
 curl -L -o "GeoLite2-ASN.mmdb.tar.gz" \
   "${BASE_URL}?edition_id=GeoLite2-ASN&license_key=${LICENSE_KEY}&suffix=tar.gz"
@@ -22,14 +30,6 @@ if [ $? -eq 0 ]; then
   echo "GeoLite2-ASN.mmdb 已成功下载并提取。"
 else
   echo "GeoLite2-ASN.mmdb 下载失败。"
-  exit 1
-fi
-
-if [ $? -eq 0 ]; then
-  tar -xvzf GeoLite2-City.mmdb.tar.gz --strip-components=1 && rm GeoLite2-City.mmdb.tar.gz
-  echo "GeoLite2-City.mmdb 已成功下载并提取。"
-else
-  echo "GeoLite2-City.mmdb 下载失败。"
   exit 1
 fi
 
