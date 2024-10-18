8182d8d4deff7daa126edb7d3d9d6194dc148dfe
alllowMultiQueries <asd1161575755@outlook.com>
2024-10-18 10:16:09 +0800

    Create geoip2_sync.sh

---
+++ docker/geoip2_sync.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# 检查是否提供了 LICENSE_KEY
+if [ -z "$LICENSE_KEY" ]; then
+  echo "错误：未设置LICENSE_KEY。请将其导出为环境变量。"
+  exit 1
+fi
+
+# 设置 MaxMind 的下载地址
+BASE_URL="https://download.maxmind.com/app/geoip_download"
+
+echo "下载 GeoLite2-City.mmdb..."
+curl -L -o "GeoLite2-City.mmdb.tar.gz" \
+  "${BASE_URL}?edition_id=GeoLite2-City&license_key=${LICENSE_KEY}&suffix=tar.gz"
+
+if [ $? -eq 0 ]; then
+  tar -xvzf GeoLite2-City.mmdb.tar.gz --strip-components=1 && rm GeoLite2-City.mmdb.tar.gz
+  echo "GeoLite2-City.mmdb 已成功下载并提取。"
+else
+  echo "GeoLite2-City.mmdb 下载失败。"
+  exit 1
+fi
+
+echo "下载 GeoLite2-ASN.mmdb..."
+curl -L -o "GeoLite2-ASN.mmdb.tar.gz" \
+  "${BASE_URL}?edition_id=GeoLite2-ASN&license_key=${LICENSE_KEY}&suffix=tar.gz"
+
+if [ $? -eq 0 ]; then
+  tar -xvzf GeoLite2-ASN.mmdb.tar.gz --strip-components=1 && rm GeoLite2-ASN.mmdb.tar.gz
+  echo "GeoLite2-ASN.mmdb 已成功下载并提取。"
+else
+  echo "GeoLite2-ASN.mmdb 下载失败。"
+  exit 1
+fi
+
+echo "下载 GeoCN.mmdb..."
+curl -L -o "GeoCN.mmdb" \
+  "http://github.com/ljxi/GeoCN/releases/download/Latest/GeoCN.mmdb"
+
+if [ $? -eq 0 ]; then
+  echo "GeoCN.mmdb 已成功下载。"
+else
+  echo "GeoCN.mmdb 下载失败。"
+  exit 1
+fi
+
+echo "所有下载已完成。"
